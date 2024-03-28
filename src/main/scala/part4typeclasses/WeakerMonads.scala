254fc993194de47ac32dcac0947a4bb8b0fbc50f
Mahesh Chand <mahesh.kndpl@gmail.com>
2024-03-28 16:49:45 +0530

    Weaker Monads hands on

---
+++ src/main/scala/part4typeclasses/WeakerMonads.scala
@@ -0,0 +1,34 @@
+package part4typeclasses
+
+import cats.{Applicative, Apply}
+
+object WeakerMonads {
+
+  //Weaker Monad = Apply + flatMap method
+  trait MyFlatMap[M[_]] extends Apply[M] {
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+
+    //Todo
+    // hint: Apply extends Functor
+    def ap[A, B](wf: M[A => B])(wa: M[A]): M[B] =
+      flatMap(wa)(a => map(wf)(f => f(a)))
+  }
+  trait MyMonad[M[_]] extends Applicative[M] with MyFlatMap[M] {
+    override def map[A, B](ma: M[A])(f: A => B): M[B] =
+      flatMap(ma)(x => pure(x))
+  }
+
+  import cats.FlatMap
+  import cats.syntax.flatMap._
+  import cats.syntax.functor._
+
+  def getPairs[M[_]: FlatMap, A, B](numbers: M[A], chars: M[B]): M[(A, B)] =
+    for {
+      n <- numbers
+      c <- chars
+    } yield (n, c)
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
